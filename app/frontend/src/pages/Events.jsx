749ee63f113dd8c1d1a7360cd042037918fb2bdd
Vilde Tveiten <vildetve@stud.ntnu.no>
2026-02-11 13:46:12 +0100

    add: search and filtering for events and listings page (#36)
    * add search and filtering on events and listings pages
    
    * fix import in listings.jsx
    
    ---------
    
    Co-authored-by: Vilde Tveiten <vildetve@ntnu.no>

--- app/frontend/src/pages/Events.jsx
+++ app/frontend/src/pages/Events.jsx
@@ -3,19 +3,66 @@ import { EventCard } from "../components/EventCard";
 import { fetchEvents } from "../services/api";
 
 
+
 export function Events() {
     const [events, setEvents] = useState([]);
+    const [filter, setFilter] = useState("All");
+    const [sortOrder, setSortOrder] = useState("newest");
+    const [searchTerm, setSearchTerm] = useState("");
+
     
     useEffect(() => {
         fetchEvents().then(data => setEvents(data));
     }, []);
 
+
+    const filteredEvents = events.filter(event => {
+        const matchesCategory = filter === "All" || event.category === filter;
+
+        const matchesSearch = event.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            event.description.toLowerCase().includes(searchTerm.toLowerCase());
+
+        return matchesCategory && matchesSearch;
+    })
+
+    .sort((a, b) => {
+        return sortOrder === "newest"
+            ? new Date(b.date) - new Date(a.date)
+            : new Date(a.date) - new Date(b.date);
+    });
+
     return (
         <>
-            <h1>Events</h1>
+            <h1 className="events-header">Events</h1>
+
+            <p className="events-subtitle">
+                Utforsk kommende arrangementer
+            </p>
+
+            <div className="events-controls">
+            <input type="text" 
+                placeholder="Søk etter arrangementer" 
+                value={searchTerm} 
+                onChange={(e) => setSearchTerm(e.target.value)} 
+                />
+
+
+            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                <option value="All">Alle kategorier</option>
+                <option value="Sosialt">Sosialt</option>
+                <option value="Sports">Sport</option>
+                <option value="Kurs">Kurs</option>
+            </select>
+
+            <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
+                <option value="newest">Nyeste først</option>
+                <option value="oldest">Eldste først</option>
+            </select>
+            </div>
+
 
             <div className="events-grid">
-                {events.map((event) => (
+                {filteredEvents.map((event) => (
                     <EventCard
                         key={event.id}
                         event={{
